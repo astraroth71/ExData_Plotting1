15ea007c2dfb708e50b74e6650ed497ac4bb11ae
Gioachino Bartolotta <gioachino.bartolotta@gmail.com>
2016-12-08 22:48:28 +0000

    created get_data.R

---
+++ get_data.R
@@ -0,0 +1,16 @@
+getData <- function(){
+        # File with data
+        datafile <- "household_power_consumption.txt"
+        # url to zip file with data
+        url_data <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        filename <- paste(Sys.Date(), "data.zip", sep = "_")
+        # check if exists the datafile
+        if(!file.exists(datafile)){
+                # if doesn't exists download the zip and extract the file
+                if(!file.exists(filename)){
+                        download.file(url_data, filename, method = "curl")
+                }
+                unzip(filename)
+        }
+        mydata <- read.table(datafile, sep=";", header=TRUE, quote= "", strip.white=TRUE, stringsAsFactors = FALSE, na.strings= "?")
+}
